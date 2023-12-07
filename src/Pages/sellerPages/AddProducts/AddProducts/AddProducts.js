3cd4c074b283ba61d05fdb1d3089573d94bc0d8f
Sarequl Basar <sarequl@proton.me>
2023-12-07 11:10:29 +0600

    all added

--- src/Pages/sellerPages/AddProducts/AddProducts/AddProducts.js
+++ src/Pages/sellerPages/AddProducts/AddProducts/AddProducts.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { AuthContext } from '../../../../contexts/AuthProvider';
 import { useForm } from 'react-hook-form';
 import { useQuery } from 'react-query';
